b56c6c7ca909ccf2c9ac41cf39c6f6390805ddfe
Fernando <fernandopl0908@gmail.com>
2023-07-24 18:43:31 -0700

    Image banner draft

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // images
-import Image from '../assets/avatar.svg';
+import Image from '../assets/me.jpg';
 // icons 
 import { FaGithub, FaYoutube, FaDribbble } from 'react-icons/fa';
 // type animation
@@ -91,7 +91,7 @@ const Banner = () => {
             variants={fadeIn('down', 0.5)}
             initial="hidden"
             whileInView={'show'}
-            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[350px]'>
             <img src={Image} alt='' />
           </motion.div>
         </div>
